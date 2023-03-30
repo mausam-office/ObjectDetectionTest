2c5a239677d6a88f5999056b461d3ed0d8c8f30f
mausam-office <eng3@ramlaxmangroup.com>
2023-03-30 14:25:50 +0545

    Shifted Boiler plate to detect function

--- TestingApp.py
+++ TestingApp.py
@@ -5,11 +5,12 @@ import numpy as np
 import streamlit as st
 
 from copy import deepcopy
+from functools import partial
 from PIL import Image
 from ultralytics import YOLO
 
 
-@st.cache_data
+@st.cache_resource
 def load_model(model_path):
     return YOLO(model_path)
 
@@ -18,7 +19,7 @@ def inference(model, img, DETECTION_THRESHOLD):
     return model.predict(source=img, conf=DETECTION_THRESHOLD)
 
 
-def postprocess(_arr):
+def postprocess(_arr, classes, img_clone):
    pts = _arr.xyxy.squeeze()
    x1, y1, x2, y2 = int(round(pts[0], 0)), int(round(pts[1], 0)), int(round(pts[2], 0)), int(round(pts[3], 0))
    score = round(float(_arr.conf) * 100, 0)
@@ -32,9 +33,26 @@ def get_extension(filename):
     return filename.split('.')[-1]
 
 
+def detect(img):
+    result = inference(model, img, DETECTION_THRESHOLD)
 
-INPUT_TYPE = ('File Upload', 'Camera')
+    boxes  = result[0].boxes
+    boxes_ = boxes.cpu().numpy()
+    classes = result[0].names
+    img = result[0].orig_img
+
+    img_clone = deepcopy(img)
+
+    # Wrap with List fuction to get the effect
+    list(map(partial(postprocess, classes=classes, img_clone=img_clone), boxes_))
+    img_clone = cv2.cvtColor(np.array(img_clone), cv2.COLOR_BGR2RGB)
+    # st.image(img_clone)
+    return img_clone
+
+
+INPUT_SOURCE_TYPE = ('File Upload', 'Camera')
 TASK_TYPE = ('Image Classification', 'Object Detection')
+INPUT_DATA_TYPE = ('Video', 'Image')
 DETECTION_THRESHOLD = 0.55
 
 model = load_model('models/best.pt')
@@ -49,20 +67,18 @@ elif task_btn==TASK_TYPE[1]:
     DETECTION_THRESHOLD = st.sidebar.slider("Detection Threshold", 0.00, 1.0, step=0.01)
 
 
+input_btn = st.sidebar.selectbox("Input Type", INPUT_SOURCE_TYPE)
 
-
-input_btn = st.sidebar.selectbox("Input Type", INPUT_TYPE)
-
-if input_btn==INPUT_TYPE[0]:    # File Upload
+if input_btn==INPUT_SOURCE_TYPE[0]:    # File Upload
     with st.expander("Details"):
         st.info("Input Source is Uploaded file.")
         st.write("You can select image or video file.")
     
-    input_data_types = ('Video', 'Image')
-    # tab_image, tab_video = st.tabs(input_data_types)
-    input_data_type = st.radio("Choose input medium.", input_data_types, key='tab_upload_input_data_type')
+    
 
-    if input_data_type==input_data_types[0]:    # video
+    input_data_type = st.radio("Choose input medium.", INPUT_DATA_TYPE, key='tab_upload_input_data_type')
+
+    if input_data_type==INPUT_DATA_TYPE[0]:    # video
         video_file = st.file_uploader(label="Upload file", type=['.mp4', '.MOV'])
         if video_file is None:
             st.stop()
@@ -77,7 +93,6 @@ if input_btn==INPUT_TYPE[0]:    # File Upload
             temp_file.write(video_file.read())
             video_path = temp_file.name
 
-            # Initialize a VideoCapture object with the video file path
             cap = cv2.VideoCapture(video_path)
             with org_frame:
                 frame_container = st.empty()
@@ -90,25 +105,11 @@ if input_btn==INPUT_TYPE[0]:    # File Upload
                     break
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 pil_img = Image.fromarray(frame)
-                result = inference(model, pil_img, DETECTION_THRESHOLD)
+                img_clone = detect(pil_img)
 
-                boxes  = result[0].boxes
-                boxes_ = boxes.cpu().numpy()
-                classes = result[0].names
-                img = result[0].orig_img
-
-                img_clone = deepcopy(img)
-
-                # Wrap with List fuction to get the effect
-                list(map(postprocess, boxes_))
-                img_clone = cv2.cvtColor(np.array(img_clone), cv2.COLOR_BGR2RGB)
-                # st.image(img_clone) 
-                # Update the container with new frame
                 frame_container.image(frame)
                 detection_container.image(img_clone)
                 
-
-
             # Release the VideoCapture object
             cap.release()
 
@@ -119,38 +120,25 @@ if input_btn==INPUT_TYPE[0]:    # File Upload
             # Delete the temporary file
             os.remove(video_path)
         
-    elif input_data_type==input_data_types[1]:  # image
+    elif input_data_type==INPUT_DATA_TYPE[1]:  # image
         file = st.file_uploader(label="Upload file", type=['.jpg', '.jpeg'])
         if file is None:
             st.stop()
         filename = file.name
         img = Image.open(file)
 
-        result = inference(model, img, DETECTION_THRESHOLD)
-
-        boxes  = result[0].boxes
-        boxes_ = boxes.cpu().numpy()
-        classes = result[0].names
-        img = result[0].orig_img
-
-        img_clone = deepcopy(img)
-
-        # Wrap with List fuction to get the effect
-        list(map(postprocess, boxes_))
-        img_clone = cv2.cvtColor(np.array(img_clone), cv2.COLOR_BGR2RGB)
+        img_clone = detect(img)
         st.image(img_clone)
         
 
-elif input_btn==INPUT_TYPE[1]:  # Camera
+elif input_btn==INPUT_SOURCE_TYPE[1]:  # Camera
         with st.expander("Details"):
             st.info("Input Source is Camera")
             st.write("Please check camera connection if any issue arises.")
         
-        input_data_types = ('Video', 'Image')
-        # tab_image, tab_video = st.tabs(input_data_types)
-        input_data_type = st.radio("Choose input medium.", input_data_types, key='tab_camera_input_data_type')
+        input_data_type = st.radio("Choose input medium.", INPUT_DATA_TYPE, key='tab_camera_input_data_type')
 
-        if input_data_type == input_data_types[0]:
+        if input_data_type == INPUT_DATA_TYPE[0]:
             st.info(f"Source type {input_data_type} is not yet implemented.")
             st.stop()
 
@@ -162,21 +150,11 @@ elif input_btn==INPUT_TYPE[1]:  # Camera
         print(f"filename is {filename} with extension {get_extension(filename)}")
         assert get_extension(filename) in ['jpg', 'jpeg', 'png']
         img = Image.open(img)
-        
-        result = inference(model, img, DETECTION_THRESHOLD)
 
-        boxes  = result[0].boxes
-        boxes_ = boxes.cpu().numpy()
-        classes = result[0].names
-        img = result[0].orig_img
-
-        img_clone = deepcopy(img)
-
-        # Wrap with List fuction to get the effect
-        list(map(postprocess, boxes_))
-        img_clone = cv2.cvtColor(np.array(img_clone), cv2.COLOR_BGR2RGB)
+        img_clone = detect(img)
         st.image(img_clone)
-
+        
+        
 
     
 
